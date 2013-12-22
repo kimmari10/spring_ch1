6a5511b5b364135f91970e8f3f352196f6985785
kimminsub <kimminsub.daum@gmail.com>
2013-12-22 18:25:57 +0900

    목표 : DaoFactory를 스프링에서 사용이 가능하도록 변신!
    해결 : Springframework라이브러리를 추가하고 , DaoFactory가 설정정보라는 표시를 위해 @Configuration이라는 어노테이션을 적용시킨다.
    그리고 오브젝트 생성 메소드라는 표시로 @Bean이라고 메서드 별로 표기한다.

--- spring_ch1/src/main/java/springbook/user/dao/UserDao.java
+++ spring_ch1/src/main/java/springbook/user/dao/UserDao.java
@@ -46,27 +46,5 @@ public class UserDao {
 		
 		return user;
 	}
-
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new DaoFactory().userDao();
-		
-		User user = new User();
-		user.setId("whiteship2");
-		user.setName("백기선2");
-		user.setPassword("married");
-		
-		dao.add(user);
-		
-		System.out.println(user.getId() + "등록성공");
-		
-		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
-		
-		System.out.println(user2.getId() + "조회 성공");
-		
-		
-		
-	}
 	
 }
