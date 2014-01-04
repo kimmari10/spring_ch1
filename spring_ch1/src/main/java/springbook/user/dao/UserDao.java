c1085b516ff1ecdff17a52bdceb23f1e208fb476
kimminsub <kimminsub.daum@gmail.com>
2014-01-04 20:06:11 +0900

    spring-jdbc의존성 추가 - maven
    java의 db연결 표준 인터페이스인
    DataSource를 구현한 스프링의 SimpeDriverDataSource 클래스를 이용하여 ,
    오라클 db연결을 해서, DaoFactory Annotation을 이용하여, 데이터 연동

--- spring_ch1/src/main/java/springbook/user/dao/UserDao.java
+++ spring_ch1/src/main/java/springbook/user/dao/UserDao.java
@@ -5,23 +5,28 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import springbook.user.domain.User;
 
 public class UserDao {
 	private ConnectionMaker connectionMaker;
-	private Connection c;
-	private User user;
-	
+	private DataSource dataSource;
+
 	public UserDao() {
 		super();
+	}
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
 
 	public void setConnectionMaker(ConnectionMaker connectionMaker) {
 		this.connectionMaker = connectionMaker;
 	}
 
-	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = this.connectionMaker.makeConnection();
+	public void add(User user) throws SQLException {
+		Connection c = this.dataSource.getConnection();
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
@@ -33,8 +38,8 @@ public class UserDao {
 		c.close();
 	}
 
-	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = this.connectionMaker.makeConnection();
+	public User get(String id) throws SQLException {
+		Connection c = this.dataSource.getConnection();
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 		
