854acdea630bbd3e1c198b6413505099e6dde46a
kimminsub <kimminsub.daum@gmail.com>
2014-01-25 16:43:16 +0900

    유저 한명에서 2명으로 바꾸고 각자의 id값으로 제대로 가져 왔는지 테스트

--- spring_ch1/src/test/java/springbook/user/dao/UserDaoTest.java
+++ spring_ch1/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -18,18 +18,23 @@ public class UserDaoTest {
 		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
+		User user = new User("len1en", "김민섭", "springno1");
+		User user2 = new User("ggeeek", "박병은", "springno2");
+		
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 		
-		User user = new User("gyumee", "박성철", "springno1");
-		
 		dao.add(user);
-		assertThat(dao.getCount(), is(1));
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
 		
-		User user2 = dao.get(user.getId());
-	
-		assertThat(user2.getName(), is(user.getName()));
-		assertThat(user2.getPassword(), is(user.getPassword()));
+		User userget1 = dao.get(user.getId());
+		assertThat(userget1.getName(), is(user.getName()));
+		assertThat(userget1.getPassword(), is(user.getPassword()));
+		
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
 		
 	}
 	
