9ac5e6bc39f4fe3ee4487d86315157abe2d160ec
kimminsub <kimminsub.daum@gmail.com>
2014-01-25 16:37:36 +0900

    1. userdao에 count갯수 가져오는 메소드와 delete메소드를 만듬 2. user생성자에 파라미터 받을 수 있도록 추가 3. userdaoTest addAndGet, count 테스트 추가

--- spring_ch1/src/test/java/springbook/user/dao/UserDaoTest.java
+++ spring_ch1/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -17,18 +17,42 @@ public class UserDaoTest {
 	public void addAndGet() throws SQLException {
 		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
-		User user = new User();
-		user.setId("gyumee");
-		user.setName("박성철");
-		user.setPassword("springno1");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		User user = new User("gyumee", "박성철", "springno1");
 		
 		dao.add(user);
+		assertThat(dao.getCount(), is(1));
 		
 		User user2 = dao.get(user.getId());
 	
 		assertThat(user2.getName(), is(user.getName()));
 		assertThat(user2.getPassword(), is(user.getPassword()));
 		
+	}
+	
+	@Test
+	public void count() throws SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		User user = new User("gyumee", "박성철", "springno2");
+		User user2 = new User("len1en", "김민섭", "springno1");
+		User user3 = new User("ggeeek", "박병은", "springno3");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
 	}
 
 }
