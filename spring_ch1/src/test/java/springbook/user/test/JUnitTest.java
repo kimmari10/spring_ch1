ce72ee02a97ebe159dc0df6806302dc9ec99a772
김민섭 <kimminsub@daumcorp.com>
2014-02-14 11:32:36 +0900

    junit을 이용한 3가지 비교 방법
    학습 테스트 - applicationContext
    
    첫 번째 방법은 assertThat() 을 이용하는 것이다. 매처와 비교할 대상인 첫 번째 파라미터에 Boolean 타입의 결과가 나오는 조건문을 넣는다.
    그리고 그 결과를 is() 매처를 써서 true와 비교하면 된다. 지금까지 시용했던 asser tThat() 과 isO를 적절히 사용한 방법이다.
    is() 는 타입만 일치하면 어떤 값이든 검증할 수 있다.
    
    두번째 방법은 조건문을 받아서 그 결과가 true인지 false인지를 확인하도록 만들어진 assertTrue() 라는 검증용 메소드를 assertThat() 대신에 사용한 것이다.
    코드가 assertThat() 방식보다는 조금 간결해지는 장점이 있다.
    
    세 번째 방법도 assertThat()을 사용한다. 이번엔 조건문을 넣어서 그 결과를 true와 비교하는 대신 매처의 조합을 이용하는 방법을 사용했다.
    either() 는 뒤에 이어서 나오는 or() 와 함께 두 개의 매처의 결과를 OR 조건으로 비교해준다. 두 가지 매처 중 에서 하나만 true로 나와도 성공이다.
    새롭게 사용한 매처가 있는데 바로 null Value()다. null Value() 는 이름 그대로 오브젝트가 null 인지를 확인해준다.

--- spring_ch1/src/test/java/springbook/user/test/JUnitTest.java
+++ spring_ch1/src/test/java/springbook/user/test/JUnitTest.java
@@ -1,7 +1,7 @@
 package springbook.user.test;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -9,10 +9,21 @@ import java.util.Set;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value={"classpath:junit.xml"})
 public class JUnitTest {
 	
+	@Autowired
+	ApplicationContext context;
+	
 	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+	static ApplicationContext contextObject = null;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -26,18 +37,28 @@ public class JUnitTest {
 	public void test() {
 		assertThat(testObjects, is(not(hasItem(this))));
 		testObjects.add(this);
+		
+		assertThat(contextObject == null || contextObject == this.context, is(true));
+		contextObject = this.context;
 	}
 	
 	@Test
 	public void test2() {
 		assertThat(testObjects, is(not(hasItem(this))));
 		testObjects.add(this);
+		
+		assertTrue(contextObject == null || contextObject == this.context);
+		contextObject = this.context;
 	}
 	
 	@Test
 	public void test3() {
 		assertThat(testObjects, is(not(hasItem(this))));
 		testObjects.add(this);
+		
+		assertThat(contextObject, 
+					  either(is(nullValue(ApplicationContext.class))).or(is(this.context)) );
+		contextObject = this.context;
 	}
 
 }
