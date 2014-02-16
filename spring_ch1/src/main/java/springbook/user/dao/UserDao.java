2e3b7e8d0c02611c37e318cd8cb4b54e543d3936
kimminsub <kimminsub.daum@gmail.com>
2014-02-16 15:51:09 +0900

    StrategyPattern을 사용하면, 바뀌는 전략에 따라 매번 클래스를 생성해야하는 번거로움이 있다. 이 부분을 어떻게 해결해야 할지 고민해보자.
    로컬 클래스 사용
    
    1.로컬 클래스는 선언된 메소드 내에서만 사용할 수 있다. 어차피 사용될 전략 클래스가
    add메소드뿐이라면, 이렇게 사용하기전에 바로 정의해서 쓰는 것도 나쁘지 않다. 덕분에 클래스 파일이 하나 줄었고
    add()메소드 안에서 PreparedStatement 생성 로직을 함께 볼 수 있으니 코드를 이해하기도 좋다.

--- spring_ch1/src/main/java/springbook/user/dao/UserDao.java
+++ spring_ch1/src/main/java/springbook/user/dao/UserDao.java
@@ -28,6 +28,23 @@ public class UserDao {
 	}
 
 	public void add(User user) throws SQLException {
+		class AddStatement implements StatementStrategy {
+			User user;
+			
+			public AddStatement(User user) {
+				super();
+				this.user = user;
+			}
+
+			public PreparedStatement makePreparedStatement(Connection c)
+					throws SQLException {
+				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+				ps.setString(1, user.getId());
+				ps.setString(2, user.getName());
+				ps.setString(3, user.getPassword());
+				return ps;
+			}
+		}
 		StatementStrategy strategy = new AddStatement(user);
 		jdbcContextWithStatementStrategy(strategy);
 	}
