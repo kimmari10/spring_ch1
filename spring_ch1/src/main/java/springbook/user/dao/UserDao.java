15e12e382a671cc2fa31c668bf2c0a70c2a702df
김민섭 <kimminsub@daumcorp.com>
2014-02-17 13:58:24 +0900

    익명내부 클래스 콜백을 다시 메소드로 별도로 분리하고, 변하는 부분과 변하지 않는 부분을 다시 나누었다. 그리고 변하는 부분은 메소드로 파라미터 전달하도록 하였다.

--- spring_ch1/src/main/java/springbook/user/dao/UserDao.java
+++ spring_ch1/src/main/java/springbook/user/dao/UserDao.java
@@ -93,45 +93,19 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
+		executeSql("delete from users");
+	}
+
+	private void executeSql(final String query) throws SQLException {
 		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
 			public PreparedStatement makePreparedStatement(Connection c)
 					throws SQLException {
-				PreparedStatement ps = c.prepareStatement("delete from users");
+				PreparedStatement ps = c.prepareStatement(query);
 				return ps;
 			}
 		});
 	}
 
-	public void jdbcContextWithStatementStrategy(StatementStrategy strategy) throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		try {
-			//예외가 발생할 가능성이 있는 코드를 모두 try블록으로 묶어준다.
-			c = dataSource.getConnection();
-			ps = strategy.makePreparedStatement(c);
-			ps.executeUpdate();
-			
-		} catch (SQLException e) {
-			//예외가 발생했을 때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다. 
-			//아직은 예외를 다시 메소드 밖으로 던지는 것 밖에 없다.
-			throw e;
-		} finally {	//finally이므로 try블록에서 예외가 발생했을 때나 안 했을 때나 모두 실행된다.
-			if(ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-					//ps.close() 에서도 SQLException이 발생 할 수 있기 때문에 이를 잡아줘야 한다. 그렇지 않으면 Connection을 close()하지 못하고 메소드를 빠져나갈 수 있다.
-				}
-			}
-			if(c != null) {
-				try {
-					c.close();	//Connection반환
-				} catch (SQLException e) {
-				}
-			}
-		}
-	}
-	
 	//메소드 추출 - 메소드를 다른 곳에 재사용 할 수 있어야 하는데, 이건 반대로 분리시키고  남은 메소드가 재사용이 필요한 부분이 되버렸다. 
 	private PreparedStatement makeStatement(Connection c) throws SQLException {
 		PreparedStatement ps;
