896e6f19c9867f60458622715169cd040b70663c
kimminsub <kimminsub.daum@gmail.com>
2014-02-18 15:18:39 +0900

    get메서드 jdbcTemplate사용하도록 수정 - queryForObject를 이용하여 rowMapper사용하여 사용자 정의 객체를 리턴 받을 수 있도록 함
    context의존성 수정
    필요없는 메서드 제거 및 수정

--- spring_ch1/src/main/java/springbook/user/dao/UserDao.java
+++ spring_ch1/src/main/java/springbook/user/dao/UserDao.java
@@ -9,12 +9,11 @@ import javax.sql.DataSource;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
 public class UserDao {
-	private ConnectionMaker connectionMaker;
 	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplate;
 	
@@ -27,113 +26,31 @@ public class UserDao {
 		this.dataSource = dataSource;
 	}
 
-	public void setConnectionMaker(ConnectionMaker connectionMaker) {
-		this.connectionMaker = connectionMaker;
-	}
-
 	public void add(final User user) throws SQLException {
 		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
 	}
 
-	public User get(String id) throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		User user = null;
-		
-		try {
-			c = this.dataSource.getConnection();
-			ps = c.prepareStatement("select * from users where id = ?");
-			ps.setString(1, id);
-			rs = ps.executeQuery();
-			if(rs.next()) {
-				user = new User();
-				user.setId(rs.getString("id"));
-				user.setName(rs.getString("name"));
-				user.setPassword(rs.getString("password"));
-			}
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
+	public User get(String id) {
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", 
+				new Object[] {id}, 
+				new RowMapper<User>() {
+					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+						User user = new User();
+						user.setId(rs.getString("id"));
+						user.setName(rs.getString("name"));
+						user.setPassword(rs.getString("password"));
+						return user;
+					}
 				}
-			}
-			if(ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if(c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
-		
-		if(user == null) {
-			throw new EmptyResultDataAccessException(1);
-		}
-		
-		return user;
+		);
 	}
 	
 	public void deleteAll() throws SQLException {
-		this.jdbcTemplate.update(new PreparedStatementCreator() {
-			public PreparedStatement createPreparedStatement(Connection con)
-					throws SQLException {
-				return con.prepareStatement("delete from users");
-			}
-		});
+		this.jdbcTemplate.update("delete from users");
 	}
 
-	//메소드 추출 - 메소드를 다른 곳에 재사용 할 수 있어야 하는데, 이건 반대로 분리시키고  남은 메소드가 재사용이 필요한 부분이 되버렸다. 
-	private PreparedStatement makeStatement(Connection c) throws SQLException {
-		PreparedStatement ps;
-		ps = c.prepareStatement("delete from users");
-		return ps;
-	}
-	
 	public int getCount() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		
-		try {
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("select count(*) from users");
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					// TODO: handle exception
-				}
-			}
-			if(ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-					// TODO: handle exception
-				}
-			}
-			if(c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-					// TODO: handle exception
-				}
-			}
-		}
+		return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
 	}
 	
 }
