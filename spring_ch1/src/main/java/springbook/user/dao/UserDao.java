9e0c6c8b3367229fd0b94014670b48b66750f8a9
kimminsub <kimminsub.daum@gmail.com>
2014-02-23 14:49:20 +0900

    중복제거 userMapper메서드는 get()과 getAll() 2군데서 전부 쓰인다. private메서드로 추출 dataSource도 이제 필요없으니까 제거.

--- spring_ch1/src/main/java/springbook/user/dao/UserDao.java
+++ spring_ch1/src/main/java/springbook/user/dao/UserDao.java
@@ -1,21 +1,17 @@
 package springbook.user.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
 public class UserDao {
-	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplate;
 	
 	public UserDao() {
@@ -24,26 +20,10 @@ public class UserDao {
 
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
-		this.dataSource = dataSource;
 	}
 
 	public void add(final User user) throws SQLException {
 		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
-	}
-
-	public User get(String id) {
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?", 
-				new Object[] {id}, 
-				new RowMapper<User>() {
-					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-				}
-		);
 	}
 	
 	public void deleteAll() throws SQLException {
@@ -54,18 +34,22 @@ public class UserDao {
 		return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
 	}
 
+	public User get(String id) {
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] {id}, this.userMapper);
+	}
+	
 	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id", new RowMapper<User>(){
-			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-				User user = new User();
-				user.setId(rs.getString("id"));
-				user.setName(rs.getString("name"));
-				user.setPassword(rs.getString("password"));
-				return user;
-			}
-		});
+		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
 	}
 
-
+	private RowMapper<User> userMapper = new RowMapper<User>(){
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+			return user;
+		}
+	};
 	
 }
