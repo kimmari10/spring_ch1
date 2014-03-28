dd072b11ecd5ae5bd7e8e42ed231d3b2b16c2896
김민섭 <kimminsub@daumcorp.com>
2014-03-28 18:42:40 +0900

    트랜잭션 기술을 추상화 시킨 PlatformTransactionManager를 이용하여 DI시켰음
    더이상 DB기술에 종속되지 않음

--- spring_ch1/src/main/java/springbook/user/service/UserService.java
+++ spring_ch1/src/main/java/springbook/user/service/UserService.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import springbook.user.dao.UserDao;
@@ -17,20 +20,19 @@ public class UserService {
 	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
 	UserDao userDao;
 	
-	private DataSource dataSource;
+	private PlatformTransactionManager transactionManager;
 	
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
+	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+		this.transactionManager = transactionManager;
 	}
 
+	
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
 	
 	public void upgradeLevels() throws Exception {
-		TransactionSynchronizationManager.initSynchronization();
-		Connection c = DataSourceUtils.getConnection(dataSource);
-		c.setAutoCommit(false);
+		TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
 		try {
 			List<User> users = userDao.getAll();
 			for(User user : users) {
@@ -38,15 +40,11 @@ public class UserService {
 					upgradeLevel(user);
 				}
 			}
-			c.commit();
+			this.transactionManager.commit(status);
 		} catch (Exception e) {
-			c.rollback();
+			this.transactionManager.rollback(status);
 			throw e;
-		} finally {
-			DataSourceUtils.releaseConnection(c, dataSource);
-			TransactionSynchronizationManager.unbindResource(this.dataSource);
-			TransactionSynchronizationManager.clearSynchronization();
-		}
+		} 
 	}
 
 	public void add(User user) {
