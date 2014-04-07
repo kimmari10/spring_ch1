827905099deb8fa4abebe78a05a4bfc1a1d99c9b
김민섭 <kimminsub@daumcorp.com>
2014-04-07 13:31:54 +0900

    Mockito라는 목프레임워크를 사용하여, 간단하게 목오브젝트 생성 후 검증 진행함
    일일이 목오브젝트를 만들 필요가 없어짐!!

--- spring_ch1/src/test/java/springbook/user/service/UserServiceTest.java
+++ spring_ch1/src/test/java/springbook/user/service/UserServiceTest.java
@@ -5,25 +5,22 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 import static springbook.user.service.UserServiceImpl.MIN_LOGCOUNT_FOR_SILVER;
 import static springbook.user.service.UserServiceImpl.MIN_RECCOMEND_FOR_GOLD;
+import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
 import java.util.List;
 
-
-
-
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import springbook.user.dao.MockUserDao;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
@@ -65,23 +62,34 @@ public class UserServiceTest {
 	public void upgradeLevels() throws Exception {
 		UserServiceImpl userServiceImpl = new UserServiceImpl();
 		
-		MockUserDao mockUserDao = new MockUserDao(this.users);
+		//UserDao 인터페이스를 구현한 테스트용 목 오브젝트는 다음과 같이 Mockito의 스태틱 메소드를 한 변 호출해주면 만들어진다.
+		UserDao mockUserDao = mock(UserDao.class);
+		
+		//mockUserDao .getAll() 이 호출됐을 때 (when) users 리스트를 리턴해주라(thenReturn)는 선언이다.
+		when(mockUserDao.getAll()).thenReturn(this.users);
 		userServiceImpl.setUserDao(mockUserDao);
 		
-		MockMailSender mockMailSender = new MockMailSender();
+		MailSender mockMailSender = mock(MailSender.class);
 		userServiceImpl.setMailSender(mockMailSender);
 		
 		userServiceImpl.upgradeLevels();
 		
-		List<User> updated = mockUserDao.getUpdated();
-		assertThat(updated.size(), is(2));
-		checkUserAndLevel(updated.get(0), "joytouch", Level.SILVER);
-		checkUserAndLevel(updated.get(1), "madnite1", Level.GOLD);
+		//User 타입의 오브젝트를 파라미터로 받으며 update( ) 메소드가 두 번 호출됐는지 (times(2)) 확인하라(verify)는 것이다.
+		verify(mockUserDao, times(2)).update(any(User.class));
 		
-		List<String> request = mockMailSender.getRequests();
-		assertThat(request.size(), is(2));
-		assertThat(request.get(0), is(users.get(1).getEmail()));
-		assertThat(request.get(1), is(users.get(3).getEmail()));
+		//verify(mockUserDao).update(users.get(1))은 users.get(1) 을 파라미터로 update( )가 호출된 적이 있는지를 확인해준다. update()가 아예 호출된 적이 없거나 파라미터가 users.get(1) 이 아니었다면 테스트는 실패한다.
+		verify(mockUserDao).update(users.get(1));
+		assertThat(users.get(1).getLevel(), is(Level.SILVER));
+		verify(mockUserDao).update(users.get(3));
+		assertThat(users.get(3).getLevel(), is(Level.GOLD));
+		
+		//MailSender 의 경우는 ArgumentCaptor라는 것을 사용해서 실제 MailSender 목 오브젝트에 전달된 파라미터를 가져와 내용을 검증하는 방법을 사용했다.
+		//파라미터를 직접 비교하기보다는 파라미터의 내부 정보를 확인해야 하는 경우에 유용하다.
+		ArgumentCaptor<SimpleMailMessage> mailMessageArg = ArgumentCaptor.forClass(SimpleMailMessage.class);
+		verify(mockMailSender, times(2)).send(mailMessageArg.capture());
+		List<SimpleMailMessage> mailMessages = mailMessageArg.getAllValues();
+		assertThat(mailMessages.get(0).getTo()[0], is(users.get(1).getEmail()));
+		assertThat(mailMessages.get(0).getTo()[0], is(users.get(3).getEmail()));
 	}
 	
 	private void checkUserAndLevel(User updated, String expectedId, Level expectedLevel) {
